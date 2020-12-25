d34db952c0eee11fce49dced41a4896b49e252ad
Merilin Ali <merilinayhan@gmail.com>
2020-12-25 22:31:38 +0300

    KombinyApp Payment

--- KombniyAppAccount/Controllers/HomeController.cs
+++ KombniyAppAccount/Controllers/HomeController.cs
@@ -48,6 +48,11 @@ namespace KombniyAppAccount.Controllers
 			return View();
 		}
 
+		public IActionResult Payment()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
